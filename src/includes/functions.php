b13e199fe1d85a217ba906f87de23fa73f484d61
Hekates <timlandolt@gmx.ch>
2024-03-16 12:09:15 +0100

    Added better error handling
    I improved the error handling by adding a sexy error banner when the db connection failed. I also saved the db connection status inside a session variable to eliminate consequential errors.

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -2,6 +2,7 @@
 include 'db.php';
 function listFullUserNames()
 {
+    if (!$_SESSION["dbConnection"]) return;
     global $connection;
     $sql = "SELECT vorname, name from benutzer";
     foreach ($connection->query($sql) as $row) {
