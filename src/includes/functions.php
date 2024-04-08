ab0145929fa75166643ae2ca834114d7a3910950
Lynnnn15 <116641057+Lynnnn15@users.noreply.github.com>
2024-04-08 19:10:37 +0200

    database connection now working but a bit janky. but working

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -20,7 +20,7 @@ function getFirstLetter(string $str)
     return substr($str, 0, 1);
 }
 
-function searchBook(string $searchInput){
+function searchBosok(string $searchInput){
     require_once 'db.php';
     if (!$_SESSION["dbConnection"]) return;
     global $connection;
@@ -28,5 +28,25 @@ function searchBook(string $searchInput){
     $sql = 'FROM buecher SELECT kurztitle, nummer WHERE kurztitle LIKE "' . $searchInput .'%"';
     foreach ($connection->query($sql) as $row) {
         echo $row['kurztitel'] . " " . $row['nummer'] . "<br/>";
+    }
+}
+function searchBook(string $searchInput){
+    $username = 'root';
+    $password = 'root';
+    $server = 'book-flow-mysql';
+    require_once 'db.php';
+    if (!$_SESSION["dbConnection"]) return;
+    global $connection;
+    $connection = new PDO("mysql:host=$server;port=3306;dbname=book_DB", $username, $password);
+    echo print_r($connection); 
+
+    $sql = 'SELECT kurztitle, nummer FROM buecher WHERE kurztitle LIKE :searchInput'; // Query
+    $statement = $connection->prepare($sql); //prepare query
+    $statement->bindValue(':searchInput', $searchInput . '%', PDO::PARAM_STR); // bind parameter
+    $statement->execute();  //execute query
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC); //fetch results
+    echo print_r($connection);
+    foreach ($results as $row) {
+        echo "Kurztitle: " . $row['kurztitle'] . ", Number: " . $row['nummer'] . "<br>";
     }
 }
