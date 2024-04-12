13cff862e2ffa55f2ac3cf6893b16356288c5b3c
timlandolt <timlandolt@gmx.ch>
2024-04-12 13:02:36 +0200

    Solved footer problem and made unimportant sites redirect to the start page

--- src/profile.php
+++ src/profile.php
@@ -16,11 +16,12 @@ if (session_status() == PHP_SESSION_NONE) {
     <link rel="stylesheet" href="assets/styles/profile.css">
     <link rel="shortcut icon" href="assets/images/BookFlow_Icon.svg" type="image/svg">
     <title>Profil | BookFlow</title>
+    <script>window.location.href = '/'</script>
 </head>
 <body class="grid-container">
 <?php include_once "templates/header.php" ?>
 <main class="grid-container">
 </main>
-<?php include_once "templates/footer.php"?>
+<?php include_once "templates/footer.php" ?>
 </body>
 </html>
