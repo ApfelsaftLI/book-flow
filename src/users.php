eb20ae38318520909baa67d994b42852b4a7739c
timlandolt <timlandolt@gmx.ch>
2024-05-24 18:47:47 +0200

    User search client side

--- src/users.php
+++ src/users.php
@@ -11,13 +11,46 @@ session_start();
     <link rel="stylesheet" href="assets/styles/users.css">
     <link rel="shortcut icon" href="assets/images/BookFlow_Icon.svg" type="image/svg">
     <title>Nutzer | BookFlow</title>
-    <script>window.location.href = "/"</script>
+
 </head>
 <body class="grid-container">
 <?php include_once "templates/header.php" ?>
 <main class="grid-container">
+    <div class="search-container">
+        <form id="search-form" method="GET" action="<?= $_SERVER['PHP_SELF'] ?>">
+            <div class="search-container">
+                <div class="search-bar-container">
+                    <input class="search-bar" type="search" name="search" id="book-search" placeholder="Suche..."
+                           value="<?= $_GET['search'] ?? '' ?>">
+                    <button class="search-icon" type="submit"></button>
+                </div>
+                <div class="dropdown">
+                    <div class="filter-container">
+                        <select class="filter" onchange="this.form.submit()"
+                                name="filter">
+                            <option value="users" <?= $_GET['filter'] == 'users' ? 'selected' : '' ?>>
+                                Nutzer
+                            </option>
 
+                            <option value="customers" <?= $_GET['filter'] == 'customers' ? 'selected' : '' ?>>
+                                Kunden
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <script>
+            //some js to not reset the filter or sort inputs
+            function updateHiddenValue(type) {
+                const selectedValue = document.querySelector(`.${type}`).value
+                document.getElementById(`${type}-hidden`).value = selectedValue
+            }
+        </script>
+
+    </div>
 </main>
-<?php include_once "templates/footer.php"?>
+<?php include_once "templates/footer.php" ?>
 </body>
 </html>
