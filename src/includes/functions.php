a8a53eb55b826c398d838f93ce8a45643c147ab6
Hekates <timlandolt@gmx.ch>
2024-05-28 21:50:11 +0200

    Customerzzz

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -1,23 +1,28 @@
 <?php
-function getProfilePicture(array $user): string {
+function getProfilePicture(array $user): string
+{
     return 'https://api.dicebear.com/8.x/initials/svg?seed=' . getFirstLetter($user["vorname"]) . getFirstLetter($user["name"]) . $user["vorname"] . $user["name"];
 }
 
-function getFirstLetter(string $str) {
+function getFirstLetter(string $str)
+{
     return substr($str, 0, 1);
 }
 
-function shortenShortTitlesShorter(array $dbResults) {
+function shortenShortTitlesShorter(array $dbResults)
+{
     $dbResults['kurztitle'] = substr($dbResults['kurztitle'], 0, 12);;
     return $dbResults;
 }
 
-function shortenAutor(array $dbResults) {
+function shortenAutor(array $dbResults)
+{
     $dbResults['autor'] = substr($dbResults['autor'], 0, 20);;
     return $dbResults;
 }
 
-function checkFileExtension($ext) {
+function checkFileExtension($ext)
+{
     if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png') {
         $pass = 1;
     } else {
@@ -26,7 +31,17 @@ function checkFileExtension($ext) {
     return $pass;
 }
 
-function reformateDate(string $date): string {
+function reformateDate(string $date): string
+{
     $dateArray = explode("-", $date);
     return implode(".", array_reverse($dateArray));
+}
+
+function validateDate($date)
+{
+    $dateArray = explode('-', $date ?? "");
+    if (count($dateArray) == 3) {
+        return checkdate($dateArray[1], $dateArray[2], $dateArray[0]);
+    }
+    return false;
 }
